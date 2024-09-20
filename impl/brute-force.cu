d866bb2246c8c923e3e24c01e679b452fd5e0a1c
Avinash Jayakar <avithemad@gmail.com>
2024-09-20 14:33:36 +0530

    Initialized repository for dbms project

---
+++ impl/brute-force.cu
@@ -0,0 +1,15 @@
+#include "data.hpp"
+#include <iostream>
+
+int main() {
+  std::string dbDir = "/media/ajayakar/space/src/tpch/data/tables/scale-1.0/";
+  std::string lineitem_file = dbDir + "lineitem.parquet";
+
+  auto lineitem_table = getArrowTable(lineitem_file);
+
+  std::cout << lineitem_table->schema()->ToString();
+
+  StringColumn* comments_column = read_string_column(lineitem_table, "comments");
+
+  std::cout << comments_column->data;
+}
